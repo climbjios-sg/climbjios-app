6ca3f015bb5d58ec00b8fbc4bc947a905752f127
En Rong <53928333+chownces@users.noreply.github.com>
2022-09-27 21:51:21 +0800

    chore(backend): enable API versioning

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
   await app.listen(3000);
 }
 bootstrap();
