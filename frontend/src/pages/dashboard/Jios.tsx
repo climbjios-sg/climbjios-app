a399631ea80266c22c809cb5f59723e819d4e417
rizhaow <rizhaow@gmail.com>
2022-10-05 17:16:19 +0800

    feat: add jio card

--- frontend/src/pages/dashboard/Jios.tsx
+++ frontend/src/pages/dashboard/Jios.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import { Box, Button, Card, CardHeader, Grid, Stack, Tab, Tabs, Typography } from '@mui/material';
+import { TabContext, TabPanel } from '@mui/lab';
 import { listJios } from '../../store/reducers/jios';
 import { useDispatch, useSelector } from '../../store';
 import Iconify from '../../components/Iconify';
 import { IconStyle } from '../../sections/@dashboard/user/profile/common';
+import { Jio } from '../../@types/jio';
+import palette from '../../theme/palette';
+import { format, addMinutes } from 'date-fns';
+import { formatStartEndDate, utcDateToLocaleDate } from '../../utils/formatTime';
 
 const StyledTab = styled(Tab)({
   '&.MuiButtonBase-root': {
@@ -12,14 +17,28 @@ const StyledTab = styled(Tab)({
   },
 });
 
-function JioCard() {
+interface JioCardProps {
+  data: Jio;
+}
+
+function JioCard({ data }: JioCardProps) {
   return (
     <Card>
-      <CardHeader title="John Doe"  subheader="@johndoe"/>
-      <Stack spacing={2} sx={{ p: 3 }}>
+      <CardHeader title={data.user.name} subheader={`@${data.user.username}`} />
+      <Stack spacing={1.5} sx={{ pl: 3, pb: 3, pt: 2 }}>
         <Stack direction="row">
-          <IconStyle icon={'eva:phone-fill'} />
-          <Typography variant="body2">{'1244'}</Typography>
+          <IconStyle icon={'mingcute:coupon-fill'} color={palette.light.grey[700]} />
+          <Typography variant="body2">
+            {data.isBuy ? `Buying ${data.numPasses} passes` : `Selling ${data.numPasses} passes`}
+          </Typography>
+        </Stack>
+        <Stack direction="row">
+          <IconStyle icon={'eva:pin-outline'} color={palette.light.grey[700]} />
+          <Typography variant="body2">{data.gym.name}</Typography>
+        </Stack>
+        <Stack direction="row">
+          <IconStyle icon={'eva:calendar-outline'} color={palette.light.grey[700]} />
+          <Typography variant="body2">{formatStartEndDate(data.startDateTime, data.endDateTime)}</Typography>
         </Stack>
       </Stack>
     </Card>
@@ -38,38 +57,55 @@ export default function Jios() {
 
   return (
     <Box sx={{ pt: 5, pb: 100, maxWidth: 600, margin: '0 auto' }}>
-      <Button
-        sx={{ borderRadius: 30, justifyContent: 'flex-start' }}
-        variant="outlined"
-        size="large"
-        color="primary"
-        fullWidth
-        startIcon={<Iconify icon="eva:search-outline" />}
-      >
-        <Typography sx={{ ml: 1, fontSize: 16 }} variant="button">
-          Search
-        </Typography>
-      </Button>
-      <Grid sx={{ pt: 1.5 }} container justifyContent="space-between" alignItems="center">
-        <Grid item>
-          <Tabs
-            value={tabValue}
-            onChange={(e, newValue) => {
-              setTabValue(newValue);
-            }}
-          >
-            {TABS.map((tab) => (
-              <StyledTab key={tab} label={tab} value={tab} />
-            ))}
-          </Tabs>
+      <TabContext value={tabValue}>
+        <Button
+          sx={{ borderRadius: 30, justifyContent: 'flex-start' }}
+          variant="outlined"
+          size="large"
+          color="primary"
+          fullWidth
+          startIcon={<Iconify icon="eva:search-outline" />}
+        >
+          <Typography sx={{ ml: 1, fontSize: 16 }} variant="button">
+            Search
+          </Typography>
+        </Button>
+        <Grid sx={{ pt: 1.5 }} container justifyContent="space-between" alignItems="center">
+          <Grid item>
+            <Tabs
+              value={tabValue}
+              onChange={(e, newValue) => {
+                setTabValue(newValue);
+              }}
+            >
+              {TABS.map((tab) => (
+                <StyledTab key={tab} label={tab} value={tab} />
+              ))}
+            </Tabs>
+          </Grid>
+          <Grid item>
+            <Button sx={{ borderRadius: 10 }} variant="outlined">
+              Refresh
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Button sx={{ borderRadius: 10 }} variant="outlined">
-            Refresh
-          </Button>
-        </Grid>
-      </Grid>
-      <Grid container sm={12}>
+        {/* Jios Tab */}
+        <TabPanel value={TABS[0]}>
+          <Grid container sm={12}>
+            <Grid sx={{ width: '100%', mt: 2 }} item>
+              {jiosData.data.map((jio) => (
+                <Grid key={jio.id} sx={{ width: '100%', mt: 2 }} item>
+                  <JioCard data={jio} />
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </TabPanel>
+        {/* Profile Tab */}
+        <TabPanel value={TABS[1]}>
+          <Grid container sm={12}></Grid>
+        </TabPanel>
+        {/* {JSON.stringify(jiosData)}
         <Grid sx={{ width: '100%', mt: 2 }} item>
           <JioCard />
         </Grid>
@@ -78,8 +114,8 @@ export default function Jios() {
         </Grid>
         <Grid sx={{ width: '100%', mt: 2 }} item>
           <JioCard />
-        </Grid>
-      </Grid>
+        </Grid> */}
+      </TabContext>
     </Box>
   );
 }
--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -5,5 +5,5 @@ export const BE_API = {
   },
   user: '/v1/user',
   gyms: '/v1/gyms',
-  posts: '/v1/posts',
+  posts: '/v1/posts/search',
 };
