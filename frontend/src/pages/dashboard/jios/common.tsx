40a3a623ce49ac3c2b966bb780301f01323165f9
rizhaow <rizhaow@gmail.com>
2022-10-06 15:29:31 +0800

    fix: jio type

--- frontend/src/pages/dashboard/jios/common.tsx
+++ frontend/src/pages/dashboard/jios/common.tsx
@@ -1,9 +1,9 @@
 import { Jio } from '../../../@types/jio';
 
 export function getPassesText(data: Jio) {
-  if (data.jioType === 'seller') {
+  if (data.type === 'seller') {
     return `Buying ${data.numPasses} passes`;
-  } else if (data.jioType === 'buyer') {
+  } else if (data.type === 'buyer') {
     return `Selling ${data.numPasses} passes`;
   } else {
     return `No need passes. Just looking for friends to climb with.`;
