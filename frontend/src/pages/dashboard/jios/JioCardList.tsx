4eef3ffaaf5d2229e60e17adbac046ab81907a2e
rizhaow <rizhaow@gmail.com>
2022-10-06 10:50:38 +0800

    feat: add jios empty and loading state

--- frontend/src/pages/dashboard/jios/JioCardList.tsx
+++ frontend/src/pages/dashboard/jios/JioCardList.tsx
@@ -1,13 +1,47 @@
 import * as React from 'react';
-import { Grid } from '@mui/material';
+import { Grid, Button } from '@mui/material';
 import { listJios } from '../../../store/reducers/jios';
 import { useDispatch, useSelector } from '../../../store';
 import JioCard from './JioCard';
 import JioCardSkeleton from './JioCardSkeleton';
+import EmptyContent from '../../../components/EmptyContent';
 
 export default function JioCardList() {
   const dispatch = useDispatch();
   const jiosData = useSelector((state) => state.jios);
+  const displayedData = React.useMemo(() => {
+    if (jiosData.error) {
+      return (
+        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+          <EmptyContent sx={{ py: 3 }} title="There's an error" />
+          <Button variant="contained">Contact Support</Button>
+        </div>
+      );
+    }
+
+    if (jiosData.loading) {
+      return (
+        <Grid sx={{ width: '100%', mt: 2 }} item>
+          <JioCardSkeleton />
+        </Grid>
+      );
+    }
+
+    if (jiosData.data.length === 0) {
+      return (
+        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+          <EmptyContent sx={{ py: 3 }} title="There's no Jios now." />
+          <Button variant="contained">Create a Jio</Button>
+        </div>
+      );
+    }
+
+    return jiosData.data.map((jio) => (
+      <Grid key={jio.id} sx={{ width: '100%', mt: 2 }} item>
+        <JioCard data={jio} />
+      </Grid>
+    ));
+  }, [jiosData.data, jiosData.error, jiosData.loading]);
 
   React.useEffect(() => {
     dispatch(listJios({}));
@@ -15,17 +49,7 @@ export default function JioCardList() {
 
   return (
     <Grid container sm={12}>
-      {jiosData.loading ? (
-        <Grid sx={{ width: '100%', mt: 2 }} item>
-          <JioCardSkeleton />
-        </Grid>
-      ) : (
-        jiosData.data.map((jio) => (
-          <Grid key={jio.id} sx={{ width: '100%', mt: 2 }} item>
-            <JioCard data={jio} />
-          </Grid>
-        ))
-      )}
+      {displayedData}
     </Grid>
   );
 }
--- frontend/src/pages/dashboard/jios/MyJioCardList.tsx
+++ frontend/src/pages/dashboard/jios/MyJioCardList.tsx
@@ -1,29 +1,56 @@
 import * as React from 'react';
-import { Grid } from '@mui/material';
+import { Button, Grid } from '@mui/material';
 import { listMyJios } from '../../../store/reducers/myJios';
 import { useDispatch, useSelector } from '../../../store';
 import MyJioCard from './MyJioCard';
+import JioCardSkeleton from './JioCardSkeleton';
+import EmptyContent from '../../../components/EmptyContent';
 
 export default function MyJioCardList() {
   const dispatch = useDispatch();
   const myJiosData = useSelector((state) => state.myJios);
+  const displayedData = React.useMemo(() => {
+    if (myJiosData.error) {
+      return (
+        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+          <EmptyContent sx={{ py: 3 }} title="There's an error" />
+          <Button variant="contained">Contact Support</Button>
+        </div>
+      );
+    }
+
+    if (myJiosData.loading) {
+      return (
+        <Grid sx={{ width: '100%', mt: 2 }} item>
+          <JioCardSkeleton />
+        </Grid>
+      );
+    }
+
+    if (myJiosData.data.length === 0) {
+      return (
+        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+          <EmptyContent sx={{ py: 3 }} title="You have no Jios." />
+          <Button variant="contained">Create a Jio</Button>
+        </div>
+      );
+    }
+
+    return myJiosData.data.map((jio) => (
+      <Grid key={jio.id} sx={{ width: '100%', mt: 2 }} item>
+        <MyJioCard data={jio} />
+      </Grid>
+    ));
+  }, [myJiosData.data, myJiosData.error, myJiosData.loading]);
 
   React.useEffect(() => {
     dispatch(listMyJios());
   }, [dispatch]);
 
-  if (myJiosData.loading) {
-    return <div>Loading...</div>
-  }
-
   return (
     <Grid container sm={12}>
       <Grid sx={{ width: '100%', mt: 2 }} item>
-        {myJiosData.data.map((jio) => (
-          <Grid key={jio.id} sx={{ width: '100%', mt: 2 }} item>
-            <MyJioCard data={jio} />
-          </Grid>
-        ))}
+        {displayedData}
       </Grid>
     </Grid>
   );
