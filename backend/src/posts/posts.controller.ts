1c3f0d530fdaf644878c7405e4a7f0e78fe74e1a
En Rong <53928333+chownces@users.noreply.github.com>
2022-10-09 21:42:45 +0800

    fix(posts-controller): fix post controller ordering

--- backend/src/posts/posts.controller.ts
+++ backend/src/posts/posts.controller.ts
@@ -27,6 +27,14 @@ export class PostController {
     return this.postService.createPost(req.user.id, body);
   }
 
+  @Get('search')
+  searchPosts(
+    @Query()
+    query: SearchPostDto,
+  ) {
+    return this.postService.searchPosts(query);
+  }
+
   @Get(':postId')
   getPost(@Req() req, @Param() params) {
     return this.postService.getPost(req.user.id, params.postId);
@@ -35,13 +43,5 @@ export class PostController {
   @Patch(':postId')
   patchPost(@Req() req, @Param() params, @Body() body: PatchPostDto) {
     return this.postService.patchPost(req.user.id, params.postId, body);
-  }
-
-  @Get('search')
-  searchPosts(
-    @Query()
-    query: SearchPostDto,
-  ) {
-    return this.postService.searchPosts(query);
   }
 }
