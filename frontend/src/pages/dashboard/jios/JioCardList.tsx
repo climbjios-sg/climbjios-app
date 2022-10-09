3731d3a32b9034d0b28e8023e4ca23f32c2b46d5
Kar Wi <t.karwi@yahoo.com>
2022-10-09 17:36:10 +0800

    feat: Add link to create jio button

--- frontend/src/pages/dashboard/jios/JioCardList.tsx
+++ frontend/src/pages/dashboard/jios/JioCardList.tsx
@@ -7,6 +7,8 @@ import useVersion from 'src/hooks/useVersion';
 import JioCard from './JioCard';
 import JioCardSkeleton from './JioCardSkeleton';
 import EmptyContent from '../../../components/EmptyContent';
+import { PATH_DASHBOARD } from 'src/routes/paths';
+import { Link } from 'react-router-dom';
 
 interface JioCardListProps {
   searchParams: ListJiosArgs;
@@ -45,7 +47,9 @@ export default function JioCardList({ searchParams }: JioCardListProps) {
               title="There's no Jios now."
               description="Create a Jio and it will show up here."
             />
-            <Button variant="contained">Create a Jio</Button>
+            <Button component={Link} to={PATH_DASHBOARD.general.jios.create} variant="contained">
+              Create a Jio
+            </Button>
           </div>
         </Grid>
       );
