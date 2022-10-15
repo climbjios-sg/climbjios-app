561d594a0dc0ec41e3c5332fafcb836200b0ff37
Kar Wi <t.karwi@yahoo.com>
2022-10-15 15:47:50 +0800

    refactor: Remove response interceptor since it was replaced by useSafeRequest

--- frontend/src/utils/authorizedAxios.ts
+++ frontend/src/utils/authorizedAxios.ts
@@ -25,31 +25,31 @@ authorizedAxios.interceptors.request.use(
 );
 
 // Response interceptor for API calls
-authorizedAxios.interceptors.response.use(
-  (response) => response,
-  async function (error) {
-    const originalRequest = error.config;
-    // If token expires, replay request
-    // TODO: to be deprecated
+// authorizedAxios.interceptors.response.use(
+//   (response) => response,
+//   async function (error) {
+//     const originalRequest = error.config;
+//     // If token expires, replay request
+//     // TODO: to be deprecated
 
-    if (error.response.status === 401 && !originalRequest._retry) {
-      // TODO: use authprovider.login()
+//     if (error.response.status === 401 && !originalRequest._retry) {
+//       // TODO: use authprovider.login()
 
-      originalRequest._retry = true;
-      const refreshToken = localStorage.getItem(REFRESH_TOKEN);
-      if (!refreshToken) {
-        return Promise.reject(error);
-      }
-      const { data } = await refreshAccessToken(refreshToken);
-      localStorage.setItem(ACCESS_TOKEN, data.accessToken);
-      localStorage.setItem(REFRESH_TOKEN, data.refreshToken);
+//       originalRequest._retry = true;
+//       const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+//       if (!refreshToken) {
+//         return Promise.reject(error);
+//       }
+//       const { data } = await refreshAccessToken(refreshToken);
+//       localStorage.setItem(ACCESS_TOKEN, data.accessToken);
+//       localStorage.setItem(REFRESH_TOKEN, data.refreshToken);
 
-      // TODO: why need this?
-      authorizedAxios.defaults.headers.common.Authorization = 'Bearer ' + data.accessToken;
-      return authorizedAxios(originalRequest);
-    }
-    return Promise.reject(error);
-  }
-);
+//       // TODO: why need this?
+//       authorizedAxios.defaults.headers.common.Authorization = 'Bearer ' + data.accessToken;
+//       return authorizedAxios(originalRequest);
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
 export default authorizedAxios;
