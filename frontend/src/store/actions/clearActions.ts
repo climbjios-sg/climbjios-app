b54aa981ad9507a1a8d092dab97132c7bc610de6
Kar Wi <t.karwi@yahoo.com>
2022-10-16 12:28:44 +0800

    refactor: Clean up

--- frontend/src/store/actions/clearActions.ts
+++ frontend/src/store/actions/clearActions.ts
@@ -1,6 +1,6 @@
 export const CLEAR_STATE = 'CLEAR_STATE';
 
-// The CLEAR_STATE action will completely reset the react-admin redux state to its initial value.
+// The CLEAR_STATE action will completely reset the redux state to its initial value.
 // This should only be called once the user has been redirected to a page which do not use the
 // state such as the login page.
 export const clearState = () => ({
