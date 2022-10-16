eddbe2384ff791d45b806c08248162e0d3c11621
Kar Wi <t.karwi@yahoo.com>
2022-10-16 15:39:11 +0800

    feat: Redirect to onboarding from login

--- frontend/src/hooks/auth/useLogin.ts
+++ frontend/src/hooks/auth/useLogin.ts
@@ -1,7 +1,7 @@
 import { useCallback } from 'react';
 import { useNavigate } from 'react-router';
 import useAuthProvider from './useAuthProvider';
-import { PATH_DASHBOARD } from '../../routes/paths';
+import { PATH_DASHBOARD, PATH_ONBOARDING } from '../../routes/paths';
 import { JwtTokenSet } from 'src/@types/token';
 
 type Login = (params: JwtTokenSet, redirectTo?: string) => Promise<any>;
@@ -11,7 +11,7 @@ const useLogin = (): Login => {
   const navigate = useNavigate();
 
   const callLogin: Login = useCallback(
-    async (params, redirectTo = PATH_DASHBOARD.root) => {
+    async (params, redirectTo = PATH_ONBOARDING.root) => {
       await authProvider.login(params);
       navigate(redirectTo);
     },
