6d9525970cbfad51ea918d7960ff016cdd849753
Kar Wi <t.karwi@yahoo.com>
2022-10-16 15:27:33 +0800

    feat: Set up skeleton code for onboarding

--- frontend/src/pages/onboarding/index.tsx
+++ frontend/src/pages/onboarding/index.tsx
@@ -1,8 +1,16 @@
+import React, { useState } from 'react';
+
 // @mui
 import { Container, Typography, Button } from '@mui/material';
 // components
 import Page from '../../components/Page';
 import Logo from '../../components/Logo';
+import { UsernameForm } from './UsernameForm';
+import { DetailsForm } from './DetailsForm';
+import { FavoriteGyms } from './FavoriteGymsForm';
+import { ClimbingGrades } from './ClimbingGradesForm';
+import { ClimbingCert } from './ClimbingCertForm';
+import { AvatarForm } from './AvatarForm';
 
 // sections
 
@@ -14,11 +22,36 @@ enum Steps {
   FavoriteGyms = 'Favorite Gyms',
   ClimbingGrades = 'Climbing Grades',
   ClimbingCert = 'Climbing Cert',
+  Avatar = 'Avatar',
 }
 
 const STEPS = Object.values(Steps);
 
+// TODO: back button
+
 export default function Onboarding() {
+  const [activeStep, setActiveStep] = useState<number>(1);
+  const isComplete = activeStep === STEPS.length;
+
+  const handleClickButton = () => {
+    if (!isComplete) {
+      setActiveStep((currentStep) => currentStep + 1);
+    } else {
+      // TODO: submit
+    }
+  };
+
+  const renderForm = () => (
+    <>
+      {activeStep === 1 && <UsernameForm />}
+      {activeStep === 2 && <DetailsForm />}
+      {activeStep === 3 && <FavoriteGyms />}
+      {activeStep === 4 && <ClimbingGrades />}
+      {activeStep === 5 && <ClimbingCert />}
+      {activeStep === 6 && <AvatarForm />}
+    </>
+  );
+
   return (
     <Page title="Onboarding: Fill in your details">
       <Container
@@ -35,6 +68,7 @@ export default function Onboarding() {
         <Typography variant="subtitle1" gutterBottom sx={{ mt: 5, mb: 1 }}>
           Fill in your details to be shown to other climbers
         </Typography>
+        {renderForm()}
         <Button
           type="submit"
           size="large"
@@ -43,6 +77,7 @@ export default function Onboarding() {
           // startIcon={submitIcon}
           fullWidth
           disableElevation
+          onClick={handleClickButton}
         >
           <Typography variant="button">{'Next'}</Typography>
         </Button>
