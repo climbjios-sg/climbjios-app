c000576de0e377f7aafa22e044368523d487f07c
Kar Wi <t.karwi@yahoo.com>
2022-10-16 11:54:32 +0800

    refactor: Clean up

--- frontend/src/@types/jio.ts
+++ frontend/src/@types/jio.ts
@@ -1,9 +1,11 @@
 import { Gym } from './gym';
 import { User } from './user';
 
+type JioType = 'seller' | 'buyer' | 'other';
+
 export interface Jio {
   id: number;
-  type: 'seller' | 'buyer' | 'other';
+  type: JioType;
   numPasses: number;
   price: number;
   gymId: number;
@@ -36,10 +38,14 @@ export type JioResponse = Jio;
 // Specific request and response types
 
 export type GetJioListRequest = Partial<Pick<Jio, 'type' | 'numPasses' | 'gymId'>> & {
-  // Get jios that end after this date string.
-  // DateTime string in ISO 8601 format
+  /**
+   * Get jios that end after this date string.
+   * DateTime string in ISO 8601 format
+   */
   startDateTime?: string;
-  // Get jios that end before this date string.
-  // DateTime string in ISO 8601 format
+  /**
+   * Get jios that end before this date string.
+   * DateTime string in ISO 8601 format
+   */
   endDateTime?: string;
 };
