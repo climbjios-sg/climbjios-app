9deea277878b13c3b2d9625ff0400a4230bcb890
En Rong <53928333+chownces@users.noreply.github.com>
2022-10-19 18:42:53 +0800

    ci: update pm2 scripts for cronjob deployment

--- backend/scripts/pm2_reload.sh
+++ backend/scripts/pm2_reload.sh
@@ -1,8 +1,22 @@
-yarn install --frozen-lockfile --production=false
-yarn build
+APPLICATION=$1
+
+BACKEND="backend"
+CRONJOB="cronjob"
+
+if [[ "$APPLICATION" != "$BACKEND" ]] && [[ "$APPLICATION" != "$CRONJOB" ]]; then
+    echo "APPLICATION must be one of '$BACKEND' | '$CRONJOB'"
+    exit 1
+fi
+
+yarn install --frozen-lockfile --production=true
 
 # Note: this is not a good way to do a rolling deployment, as db is updated first before
 # any instance is ready to use the new schema. Also, there is no rollback if migration fails,
 # or rolling deployment fails. We will handle any errors manually for now.
 yarn migrate
-pm2 reload "climbjios-backend-${NODE_ENV}"
+
+if [[ "$APPLICATION" == "$BACKEND" ]]; then
+    pm2 reload "climbjios-backend-${NODE_ENV}"
+elif [[ "$APPLICATION" == "$CRONJOB" ]]; then
+    pm2 reload "climbjios-cronjob-${NODE_ENV}"
+fi
