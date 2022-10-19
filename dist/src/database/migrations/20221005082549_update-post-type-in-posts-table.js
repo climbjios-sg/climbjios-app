bcebd6bcf73767837fb15ff8b027f714ea7f7a18
chownces <53928333+chownces@users.noreply.github.com>
2022-10-19 12:23:29 +0000

    Build: (f8a3fdf) ci: fix ci scripts

--- dist/src/database/migrations/20221005082549_update-post-type-in-posts-table.js
+++ dist/src/database/migrations/20221005082549_update-post-type-in-posts-table.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.down = exports.up = void 0;
-const types_1 = require("../../utils/types");
 async function up(knex) {
     return knex.schema
         .table('posts', (t) => {
@@ -9,14 +8,8 @@ async function up(knex) {
     })
         .then(() => knex.select().from('posts'))
         .then((posts) => Promise.all(posts.map((p) => p.isBuy
-        ? knex
-            .update({ type: types_1.PostType.BUYER })
-            .from('posts')
-            .where('id', p.id)
-        : knex
-            .update({ type: types_1.PostType.SELLER })
-            .from('posts')
-            .where('id', p.id))))
+        ? knex.update({ type: 'buyer' }).from('posts').where('id', p.id)
+        : knex.update({ type: 'seller' }).from('posts').where('id', p.id))))
         .then(() => knex.raw(`ALTER TABLE posts ALTER COLUMN type SET NOT NULL;`))
         .then(() => knex.schema.table('posts', (t) => {
         t.dropColumn('isBuy');
