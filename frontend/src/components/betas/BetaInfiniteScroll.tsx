34b2b91818f1c99b1555d5a614c7737352d683ea
rizhaow <rizhaow@gmail.com>
2022-10-25 18:54:44 +0800

    fix: move overflow css

--- frontend/src/components/betas/BetaInfiniteScroll.tsx
+++ frontend/src/components/betas/BetaInfiniteScroll.tsx
@@ -6,7 +6,6 @@ import { AxiosResponse } from 'axios';
 import BetaCard from './BetaCard';
 import BetaLoader from './BetaLoader';
 import MyBetaCard from './MyBetaCard';
-import './betaInfiniteScroll.css'
 
 const StyledInfiniteScroll = styled(InfiniteScroll)({
   maxWidth: 600,
