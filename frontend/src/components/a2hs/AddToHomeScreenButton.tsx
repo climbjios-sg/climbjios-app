781541ef2fc07e0f9d78937d759520fdc50c5a67
kwokieee <bryankwokyf@gmail.com>
2022-10-26 10:09:20 +0800

    feat: add to home screen instruction button

---
+++ frontend/src/components/a2hs/AddToHomeScreenButton.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import { DialogAnimate } from "src/components/animate";
+import { Button, Stack, Typography } from '@mui/material';
+import Iconify from 'src/components/Iconify';
+import { InlineIcon } from '@iconify/react';
+import { isiOS, isPWA, isMobile } from 'src/utils/device';
+
+export default function AddToHomeScreenButton() {
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+  const handleOpenModal = () => setIsOpenModal(true);
+  const handleCloseModal = () => setIsOpenModal(false);
+  // Application has already been installed, or user is accessing from a non-mobile device
+  // Install button should not be displayed
+  if (isPWA() || !isMobile()) {
+    return <></>;
+  }
+
+  return (
+    <>
+      <Button
+        startIcon={<Iconify icon="bxs:download" />}
+        variant={'outlined'}
+        onClick={handleOpenModal}
+        fullWidth
+      >
+        Add ClimbJios to your homescreen!
+      </Button>
+      <DialogAnimate open={isOpenModal} onClose={handleCloseModal}>
+        {
+        isiOS()
+          ? <Stack spacing={3} sx={{ p: 3 }}>
+              <Typography>
+                Add the ClimbJios app to your device's homescreen for easy access anytime.
+              </Typography>
+              <Stack spacing={1}>
+                <Typography>
+                  1. Tap on <InlineIcon icon="uil:upload-alt" />
+                </Typography>
+                <Typography>2. Select Add to Home Screen <InlineIcon icon="fluent:add-square-24-regular" /></Typography>
+              </Stack>
+            </Stack>
+          : <Stack spacing={3} sx={{ p: 3 }}>
+              <Typography>
+                Add the ClimbJios app to your device's homescreen for easy access anytime.
+              </Typography>
+              <Stack spacing={1}>
+                <Typography>
+                  1. Tap on <InlineIcon icon="carbon:overflow-menu-vertical" />
+                </Typography>
+                <Typography>2. Select <InlineIcon icon="material-symbols:add-to-home-screen" /> Install or <InlineIcon icon="material-symbols:add-to-home-screen" /> Install App</Typography>
+              </Stack>
+              <Typography>
+                For older devices, tap on <InlineIcon icon="ci:home-alt-plus" /> next to the URL bar.
+              </Typography>
+            </Stack>
+        }
+      </DialogAnimate>
+    </>
+  );
+}
--- frontend/src/utils/device.ts
+++ frontend/src/utils/device.ts
@@ -14,3 +14,24 @@ export function isiOS() {
 export function isPWA() {
   return window.matchMedia('(display-mode: standalone)').matches;
 }
+
+export function isMobile() {
+  let hasTouchScreen = false;
+  if ("maxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.maxTouchPoints > 0;
+  } else {
+    const mQ = matchMedia?.("(pointer:coarse)");
+    if (mQ?.media === "(pointer:coarse)") {
+      hasTouchScreen = !!mQ.matches;
+    } else if ("orientation" in window) {
+      hasTouchScreen = true; // deprecated, but good fallback
+    } else {
+      // Only as a last resort, fall back to user agent sniffing
+      const UA = navigator.userAgent;
+      hasTouchScreen =
+        /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+        /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+    }
+  }
+  return hasTouchScreen;
+}
