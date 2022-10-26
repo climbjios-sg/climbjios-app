d854ccb4547f1d1dd76375732f7fdfc10cab76b1
Kar Wi <t.karwi@yahoo.com>
2022-10-26 19:27:30 +0800

    fix: Fix race condition using mutex

--- frontend/src/utils/authorizedAxios.ts
+++ frontend/src/utils/authorizedAxios.ts
@@ -3,11 +3,13 @@ import { refreshAccessToken } from 'src/services/token';
 // config
 import { HOST_API } from '../config';
 import { ACCESS_TOKEN, REFRESH_TOKEN } from './jwt';
+import { Mutex } from 'async-mutex';
 
 // ----------------------------------------------------------------------
 
 export const baseAxios = axios.create();
 
+const mutex = new Mutex();
 const authorizedAxios = axios.create({
   baseURL: HOST_API,
 });
@@ -28,30 +30,31 @@ authorizedAxios.interceptors.request.use(
 
 authorizedAxios.interceptors.response.use(
   (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-    // If token expires, replay request
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
+  (error) =>
+    mutex.runExclusive(async () => {
+      const originalRequest = error.config;
+      // If token expires, replay request
+      if (error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
 
-      const refreshToken = localStorage.getItem(REFRESH_TOKEN);
-      if (!refreshToken) {
-        throw error;
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+        if (!refreshToken) {
+          throw error;
+        }
+
+        try {
+          const res = await refreshAccessToken(refreshToken);
+          localStorage.setItem(ACCESS_TOKEN, res.data.accessToken);
+          localStorage.setItem(REFRESH_TOKEN, res.data.refreshToken);
+        } catch {
+          throw error;
+        }
+
+        return authorizedAxios(originalRequest);
       }
 
-      try {
-        const res = await refreshAccessToken(refreshToken);
-        localStorage.setItem(ACCESS_TOKEN, res.data.accessToken);
-        localStorage.setItem(REFRESH_TOKEN, res.data.refreshToken);
-      } catch {
-        throw error;
-      }
-
-      return authorizedAxios(originalRequest);
-    }
-
-    throw error;
-  }
+      throw error;
+    })
 );
 
 export default authorizedAxios;
