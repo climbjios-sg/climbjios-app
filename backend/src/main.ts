3712769573363d3adea75eb0dc74ae835287f772
rizhaow <rizhaow@gmail.com>
2022-10-30 15:52:24 +0800

    fix: be changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    // origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     optionsSuccessStatus: 200,
   });
   await app.listen(process.env.PORT || 4000);
