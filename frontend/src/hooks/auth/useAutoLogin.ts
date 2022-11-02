9c273e76abcfea624399e90487866626e87b1b4f
Kar Wi <t.karwi@yahoo.com>
2022-11-02 22:13:46 +0800

    fix: redirect on login

--- frontend/src/hooks/auth/useAutoLogin.ts
+++ frontend/src/hooks/auth/useAutoLogin.ts
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { ACCESS_TOKEN, REFRESH_TOKEN } from 'src/utils/jwt';
 import useCustomSnackbar from '../useCustomSnackbar';
+import useRedirectPath from '../useRedirectPath';
 import useLogin from './useLogin';
 
 /**
@@ -12,6 +13,7 @@ const useAutoLogin = () => {
   const login = useLogin();
   const [searchParams] = useSearchParams();
   const { enqueueError } = useCustomSnackbar();
+  const { redirectPath, clearRedirectPath } = useRedirectPath();
 
   useEffect(() => {
     const callLogin = async () => {
@@ -23,17 +25,29 @@ const useAutoLogin = () => {
       }
 
       try {
-        await login({
-          accessToken,
-          refreshToken,
-        });
+        await login(
+          {
+            accessToken,
+            refreshToken,
+          },
+          redirectPath?.to,
+          redirectPath?.options
+        );
+        clearRedirectPath();
       } catch {
         enqueueError('Failed to login using the tokens in the url params.');
       }
     };
 
     callLogin();
-  }, [enqueueError, login, searchParams]);
+  }, [
+    clearRedirectPath,
+    enqueueError,
+    login,
+    redirectPath?.options,
+    redirectPath?.to,
+    searchParams,
+  ]);
 };
 
 export default useAutoLogin;
