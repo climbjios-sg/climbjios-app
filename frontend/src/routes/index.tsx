275e1cadf2582fee4205f3654820f4320e01abbe
rizhaow <rizhaow@gmail.com>
2022-11-02 23:48:35 +0800

    fix: auth redir

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -66,7 +66,7 @@ export default function Router() {
       element: <JioPage />,
     },
     {
-      path: '',
+      path: 'authRedirect',
       element: <AutoLogin />,
     },
     { path: '*', element: <Navigate to="/login" replace /> },
