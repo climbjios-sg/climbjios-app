32e7a58c8def8670117cb1e4230ddc41fa2fb2e4
Toh Hong Xiang <tohhongxiang@gmail.com>
2022-12-02 01:18:04 +0800

    feature: Add button on public climber page to message them directly

--- frontend/src/components/profile/ProfileHeader.tsx
+++ frontend/src/components/profile/ProfileHeader.tsx
@@ -1,7 +1,9 @@
-import { Skeleton, Stack, Typography } from '@mui/material';
+import { Button, Link, Skeleton, Stack, Typography } from '@mui/material';
 import { User } from 'src/@types/user';
 import { PronounName } from 'src/@types/pronoun';
 import NameAvatar from '../NameAvatar';
+import Iconify from '../Iconify';
+import useGetIdentity from 'src/hooks/auth/useGetIdentity';
 
 type ProfileHeaderProps = {
   user: User;
@@ -9,8 +11,10 @@ type ProfileHeaderProps = {
 };
 
 export default function ProfileHeader({ user, loading = false }: ProfileHeaderProps) {
+  const { identity: loggedInUser } = useGetIdentity();
+  const isLookingAtOwnProfile = user.userId === loggedInUser.userId
   return (
-    <Stack direction="row" alignItems="center">
+    <Stack direction="row" alignItems={{ xs: isLookingAtOwnProfile ? "center" : "flex-start", sm: "center" }}>
       {loading ? (
         <Skeleton variant='circular' sx={{ width: 60, height: 60 }} />
       ) : (
@@ -26,25 +30,32 @@ export default function ProfileHeader({ user, loading = false }: ProfileHeaderPr
           }}
         />
       )}
-      <Stack direction="column">
-        {loading ? (
-          <Skeleton width={150} sx={{ ml: 3 }} />
-        ) : (
-          <>
-            <Stack direction="row" alignItems="center" spacing={1}>
-              <Typography variant="subtitle1">{user.name}</Typography>
-              {user.pronoun && user.pronoun.name !== PronounName.PreferNotToSay && (
-                <Typography
-                  sx={{ color: 'text.secondary' }}
-                  variant="body2"
-                >{`(${user.pronoun.name})`}</Typography>
-              )}
-            </Stack>
-            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              {`@${user.telegramHandle}`}
-            </Typography>
-          </>
-        )}
+      <Stack direction={{ xs: "column", sm: "row" }} justifyContent="space-between" alignItems={{ xs: isLookingAtOwnProfile ? "center" : "flex-start", sm: "center" }} flexWrap="wrap" width={isLookingAtOwnProfile ? "auto" : "100%"}>
+        <Stack direction="column">
+          {loading ? (
+            <Skeleton width={150} sx={{ ml: 3 }} />
+          ) : (
+            <>
+              <Stack direction="row" alignItems="center" spacing={1}>
+                <Typography variant="subtitle1">{user.name}</Typography>
+                {user.pronoun && user.pronoun.name !== PronounName.PreferNotToSay && (
+                  <Typography
+                    sx={{ color: 'text.secondary' }}
+                    variant="body2"
+                  >{`(${user.pronoun.name})`}</Typography>
+                )}
+              </Stack>
+              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                {`@${user.telegramHandle}`}
+              </Typography>
+            </>
+          )}
+        </Stack>
+        {!isLookingAtOwnProfile && <Link href={`https://t.me/${user.telegramHandle}`} underline="none" target="_blank" rel="noopener noreferrer">
+          <Button variant="outlined" style={{ margin: '5px 0' }}>
+            <Iconify icon={'jam:telegram'} color={'#2aabee'} />
+          </Button>
+        </Link>}
       </Stack>
     </Stack>
   );
