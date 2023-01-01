c2ebcbc7ca63cf264bcd1fee8cadef659191d1a9
nathantew14 <nathantew14@gmail.com>
2023-01-01 23:14:27 +0800

    Revert "tryna figure out how to prevent reloading of page every time"
    This reverts commit 15e383030725cf02ec814fdeffbfdd681b25d2fe.

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -6,8 +6,6 @@ import {
 } from 'react-redux';
 import { CLEAR_STATE } from './actions/clearActions';
 import _rootReducer from './reducers';
-// @ts-ignore
-// import logger from 'redux-logger'
 
 const rootReducer: Reducer = (state: RootState, action: AnyAction) => {
   if (action.type === CLEAR_STATE) {
@@ -19,7 +17,6 @@ const rootReducer: Reducer = (state: RootState, action: AnyAction) => {
 
 export const store = configureStore({
   reducer: rootReducer,
-  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export type RootState = ReturnType<typeof _rootReducer>;
