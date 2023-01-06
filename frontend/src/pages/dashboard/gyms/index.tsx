592512033e77cbf3301c53b32335208547594d69
nathantew14 <nathantew14@gmail.com>
2023-01-06 15:56:00 +0800

    feat: added initialValue prop so search string can be remembered

--- frontend/src/pages/dashboard/gyms/index.tsx
+++ frontend/src/pages/dashboard/gyms/index.tsx
@@ -3,12 +3,12 @@ import useAddToHomeScreen from 'src/hooks/useAddToHomeScreen';
 import GymSearch from './GymSearch';
 import FloatingContainer from '../../../components/FloatingContainer';
 import GymCardsList from './list/GymCardsList';
-import { useState } from 'react';
+import { useSessionStorageState } from 'ahooks';
 
 export default function Gyms() {
   useAddToHomeScreen();
 
-  const [searchString, setSearchString] = useState('');
+  const [searchString, setSearchString] = useSessionStorageState('GymCardsSearch', { defaultValue: '' });
 
   return (
     <Box sx={{ pt: 2, pb: 20, minHeight: '100vh', maxWidth: 600, margin: '0 auto' }}>
@@ -24,7 +24,7 @@ export default function Gyms() {
             borderRadius: 0,
           }}
         >
-          <GymSearch setSearchString={setSearchString} />
+          <GymSearch initialValue={searchString} setSearchString={setSearchString} />
           {/* <TestComp/> */}
         </Paper>
       </FloatingContainer>
