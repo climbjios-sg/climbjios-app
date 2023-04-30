5300f268bc7b99e6ec96036f73c29158755ad00f
nathantew14 <nathantew14@gmail.com>
2023-05-01 00:03:59 +0800

    feat: tweaked disclaimers

--- frontend/src/pages/gymDetailsPage/GymDetailsPage.tsx
+++ frontend/src/pages/gymDetailsPage/GymDetailsPage.tsx
@@ -42,6 +42,12 @@ export default function GymDetailsPage() {
     { label: 'rates', component: <GymPassesTab gymId={gymDetails.id} /> },
   ];
 
+  const disclaimers = [
+    "❗️ Disclaimer: the information here is collected manually, and is not guaranteed to be accurate. Do refer to the gyms' websites for full details.",
+    '',
+    "❗️ Disclaimer: the information here is collected manually, and is not guaranteed to be accurate or exhaustive. Prices shown may also be before GST. Do refer to the gyms' websites for full details.",
+  ];
+
   return (
     <Stack>
       {GymPageHeader(gymDetails)}
@@ -58,12 +64,9 @@ export default function GymDetailsPage() {
           ))}
         </Tabs>
       </Box>
-      {currentTab !== 1 && (
+      {disclaimers[currentTab] !== '' && (
         <Card sx={{ padding: 1, pl: 2, pr: 2, ml: 2, mr: 2, mt: 1 }}>
-          <Typography fontSize={10}>
-            ❗️ Disclaimer: the information here is collected manually, and is not guaranteed to be
-            accurate or exhaustive. Do refer to the gyms' websites for full details.
-          </Typography>
+          <Typography fontSize={10}>{disclaimers[currentTab]}</Typography>
         </Card>
       )}
       {tabs[currentTab].component}
